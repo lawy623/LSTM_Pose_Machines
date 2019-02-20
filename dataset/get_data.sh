68a92122272d5615f8e2761a8a5631ea4d405ed2
lawy623 <lawy623@gmail.com>
2019-02-20 07:49:39 -0500

    new Penn Address

--- dataset/get_data.sh
+++ dataset/get_data.sh
@@ -14,5 +14,6 @@ rm -f sub_splits.zip
 rm -f puppet_mask.zip
 
 # get PENN
-wget -O Penn_Action.tar.gz https://public.boxcloud.com/d/1/b1!0ObleMBJzNK-uvDftp74Yto2WKLYfsS-Cj0T-HqNm8LqqeKEALQr2KOx45FYfH2_F301z1wcNV5g2ilToMFf5fbLjQ4ubYgMxeEO_WYW3JDm8DMKC7ZS7t-BB63K9nV-FmHdOEOgkL1iwJy7C4qKifzosUK3wVZeiM_0YAuLwN24O28dawxOCZsKJR-7XobML-UdEMmMrlU-AY8hBFYKyrK4425uvmCxnIFU91bS9EQnUeNG_DV_9hrIkvfwSqw2ze4Q9oToStar -zxvf Penn_Action.tar.gz -C PENN/
+wget -O Penn_Action.tar.gz https://public.boxcloud.com/d/1/b1!0ObleMBJzNK-uvDftp74Yto2WKLYfsS-Cj0T-HqNm8LqqeKEALQr2KOx45FYfH2_F301z1wcNV5g2ilToMFf5fbLjQ4ubYgMxeEO_WYW3JDm8DMKC7ZS7t-BB63K9nV-FmHdOEOgkL1iwJy7C4qKifzosUK3wVZeiM_0YAuLwN24O28dawxOCZsKJR-7XobML-UdEMmMrlU-AY8hBFYKyrK4425uvmCxnIFU91bS9EQnUeNG_DV_9hrIkvfwSqw2ze4Q9oToS
+tar -zxvf Penn_Action.tar.gz -C PENN/
 rm -f Penn_Action.tar.gz
